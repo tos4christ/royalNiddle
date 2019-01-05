cf71501b20a86536e13a838a16276251021edbba
Tos4christ <glmmission@hotmail.com>
2019-01-05 20:05:47 +0100

    Update callback url for facebook strategy

--- app.js
+++ app.js
@@ -51,14 +51,14 @@ function generateOrFindUser(accessToken, refreshToken, profile, done) {
 passport.use(new GithubStrategy({
 	clientID: process.env.GITHUB_CLIENT_ID,
 	clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	callbackURL: "https://royalniddle.herookuapp.com/OauthUsers/github/callback"
+	callbackURL: "https://royalniddle.herokuapp.com/OauthUsers/github/callback"
 }, generateOrFindUser));
 
 // Configure Facebook Strategy
 passport.use(new FacebookStrategy({
 	clientID: process.env.FACEBOOK_APP_ID,
 	clientSecret: process.env.FACEBOOK_APP_SECRET,
-	callbackURL: "https://royalniddle.herookuapp.com/OauthUsers/facebook/callback",
+	callbackURL: "https://royalniddle.herokuapp.com/OauthUsers/facebook/callback",
 	profileFields: ['id', 'displayName', 'photos', 'email']
 }, generateOrFindUser));
 
