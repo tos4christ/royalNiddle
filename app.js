04646c83ca14a748e80f047e8af13cbf3656266a
Tos4christ <glmmission@hotmail.com>
2019-01-05 10:16:54 +0100

    Updated host from local host to heroku

--- app.js
+++ app.js
@@ -51,14 +51,14 @@ function generateOrFindUser(accessToken, refreshToken, profile, done) {
 passport.use(new GithubStrategy({
 	clientID: process.env.GITHUB_CLIENT_ID,
 	clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	callbackURL: "http://localhost:3000/OauthUsers/github/callback"
+	callbackURL: "https://royalniddle.herookuapp.com/OauthUsers/github/callback"
 }, generateOrFindUser));
 
 // Configure Facebook Strategy
 passport.use(new FacebookStrategy({
 	clientID: process.env.FACEBOOK_APP_ID,
 	clientSecret: process.env.FACEBOOK_APP_SECRET,
-	callbackURL: "http://localhost:3000/OauthUsers/facebook/callback",
+	callbackURL: "https://royalniddle.herookuapp.com/OauthUsers/facebook/callback",
 	profileFields: ['id', 'displayName', 'photos', 'email']
 }, generateOrFindUser));
 
