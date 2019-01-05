d50b306d7f79b2aaff0453165efe2b5380a04236
Tos4christ <glmmission@hotmail.com>
2019-01-05 21:03:55 +0100

    updated proxy for strategy

--- app.js
+++ app.js
@@ -51,15 +51,17 @@ function generateOrFindUser(accessToken, refreshToken, profile, done) {
 passport.use(new GithubStrategy({
 	clientID: process.env.GITHUB_CLIENT_ID,
 	clientSecret: process.env.GITHUB_CLIENT_SECRET,
-	callbackURL: "https://royalniddle.herokuapp.com/OauthUsers/github/callback"
+	callbackURL: "/OauthUsers/github/callback",
+	proxy: true
 }, generateOrFindUser));
 
 // Configure Facebook Strategy
 passport.use(new FacebookStrategy({
 	clientID: process.env.FACEBOOK_APP_ID,
 	clientSecret: process.env.FACEBOOK_APP_SECRET,
-	callbackURL: "https://royalniddle.herokuapp.com/OauthUsers/facebook/callback",
-	profileFields: ['id', 'displayName', 'photos', 'email']
+	callbackURL: "/OauthUsers/facebook/callback",
+	profileFields: ['id', 'displayName', 'photos', 'email'],
+	proxy: true
 }, generateOrFindUser));
 
 
