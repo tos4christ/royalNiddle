0c1e6eef6a8134b317e952c973cbb83d24cc54e6
Tos4christ <glmmission@hotmail.com>
2019-01-06 22:04:34 +0100

    updated code to show profile information if callback call is successful

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var usersRouter = require('./routes/users');
 var OauthUsers = require('./routes/OauthUsers');
 
 function generateOrFindUser(accessToken, refreshToken, profile, done) {
+	console.log(profile);
 	if(profile.emails[0]) {
 
 		User.findOneAndUpdate(
