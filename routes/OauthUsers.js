47de2750f927d5f0c5a599d61dbc4da139bfff7e
Tos4christ <glmmission@hotmail.com>
2019-01-06 15:48:25 +0100

    update from http to https

--- routes/OauthUsers.js
+++ routes/OauthUsers.js
@@ -2,13 +2,16 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
+function getHost(req, res) {
+	console.log(req);
+}
 
 //GET /github/login
-router.get('/github/login', 
+router.get('/github/login', getHost, 
 	passport.authenticate('github'));
 
 // GET /github/callback
-router.get('/github/callback', 
+router.get('/github/callback', getHost,
 	passport.authenticate('github', {failureRedirect: '/'}), 
 	function(req, res) {
 		// Success Auth, redirect profile page
@@ -16,12 +19,12 @@ router.get('/github/callback',
 	});
 
 // GET /facebook/login
-router.get('/facebook/login', 
+router.get('/facebook/login', getHost,
 	passport.authenticate('facebook', {scope: ["email"]}));
 
 
 // GET /facebook/callback
-router.get('/facebook/callback', 
+router.get('/facebook/callback', getHost,
 	passport.authenticate('facebook', {failureRedirect: '/'}),
 	function(req, res) {
 		// Success Auth, redirect profile page
